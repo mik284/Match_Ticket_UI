de3f4b63fa8c6f5bd9616469aba343f121551e70
ngechu <29402779+ngechu@users.noreply.github.com>
2025-08-25 14:20:18 +0300

    Add graphql  util

---
+++ src/utils/graphqlUtils.js
@@ -0,0 +1,44 @@
+import { GraphQLClient } from 'graphql-request';
+import { useQuery } from 'react-query';
+
+export function fetchDataWithUseQuery({ key, query, variables = {} }) {
+  const config = { refetchOnWindowFocus: false };
+  const endpoint = `${BASE_URL}`;
+  const headers = {
+    headers: {
+      authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  };
+
+  const client = new GraphQLClient(`${endpoint}/graphql`, headers);
+
+  return useQuery(key, async () => await client.request(query, variables), config);
+}
+
+// A handler to perform secure mutations with authentication token
+export const mutationHandler = async (values, mutationQuery, token = null) => {
+  const headers = {
+    headers: {
+      authorization: token ? `Bearer ${token}` : `Bearer ${localStorage.getItem('token')}`,
+    },
+  };
+  const endpoint = `${BASE_URL}`;
+  const client = new GraphQLClient(`${endpoint}/graphql`, headers);
+  return await client.request(mutationQuery, {
+    input: values,
+  });
+};
+
+// A handler to fetch data and returns a promise with authentication token
+export async function asyncFetchData({ query, variables = {} }) {
+  const endpoint = `${BASE_URL}`;
+  const headers = {
+    headers: {
+      authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  };
+
+  const client = new GraphQLClient(`${endpoint}/graphql`, headers);
+
+  return await client.request(query, variables);
+}
